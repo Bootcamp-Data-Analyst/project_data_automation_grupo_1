8785e8ec280652c7c206a99b824dec44f427e02f
Alejandrocg1702 <alejandrocg1702@gmail.com>
2025-12-04 14:06:58 +0100

    limpieza de datos, creacion de tablas dinamicas, creacion de dashboard y un boton intuitivo y muy amigable para que puedan observarlo en pantalla completa.

--- src/config.py
+++ src/config.py
@@ -13,7 +13,7 @@ load_dotenv(dotenv_path)
 
 # === CONFIGURACIÓN DE BASE DE DATOS ===
 DB_USER = os.getenv('DB_USER', 'root')
-DB_PASSWORD = os.getenv('DB_PASSWORD', '')
+DB_PASSWORD = os.getenv('DB_PASSWORD', '8222')
 DB_HOST = os.getenv('DB_HOST', 'localhost')
 DB_PORT = os.getenv('DB_PORT', '3306')
 DB_NAME = os.getenv('DB_NAME', 'sakila')
